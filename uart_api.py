1c7ad79a26d41d0a674b5d02176ad9736d5f94fb
Taylor Berry <berrytc1@appstate.edu>
2019-05-30 15:12:42 -0400

    added get_closer_color

--- uart_api.py
+++ uart_api.py
@@ -1,29 +1,55 @@
+import numpy as np
 class uart_api:
-    	def __init__():
+    available_pixel = {}
+    def __init__(self):
+        self.available_pixel = {'red':[255,0,0], 'green':[0,255,0], 'blue':[0,0,255],'magenta':[255,0,255], 'tomato':[255,99,71], 'lawn green':[124,252,0]}
 
 
-	#
-	#	HEAT MAP
-	#
-	def generate_heatmap(image, canvas)
-		image = image.astype(dtype='int32')
-		canvas = canvas.astype(dtype='int32')
+#
+#	HEAT MAP
+#
+    def generate_heatmap(image, canvas):
+        image = image.astype(dtype='int32')
+        canvas = canvas.astype(dtype='int32')
 
-		subtraction = np.subtract(image,canvas)
-		print(subtraction)
+        subtraction = np.subtract(image,canvas)
+        print(subtraction)
 
-		im = np.asarray(Image.open('images/Future-human-Face.png').convert('RGB')) 
-		heatmap = np.full(im.shape,255, dtype='uint8')
+        im = np.asarray(Image.open('images/Future-human-Face.png').convert('RGB'))
+        heatmap = np.full(im.shape,255, dtype='uint8')
 
-		print(heatmap.shape)
-		for i in range(subtraction.shape[0]):
-			for j in range(subtraction.shape[1]):
-				if (subtraction[i][j] < 0):
-					heatmap[i][j][0] -= abs(subtraction[i][j])
-					heatmap[i][j][1] -= abs(subtraction[i][j])
-				elif (subtraction[i][j] > 0):
-					heatmap[i][j][2] -= abs(subtraction[i][j])
-					heatmap[i][j][1] -= abs(subtraction[i][j])
-		return heatmap
+        print(heatmap.shape)
+        for i in range(subtraction.shape[0]):
+            for j in range(subtraction.shape[1]):
+                if (subtraction[i][j] < 0):
+                    heatmap[i][j][0] -= abs(subtraction[i][j])
+                    heatmap[i][j][1] -= abs(subtraction[i][j])
+                elif (subtraction[i][j] > 0):
+                    heatmap[i][j][2] -= abs(subtraction[i][j])
+                    heatmap[i][j][1] -= abs(subtraction[i][j])
+
+        return heatmap
+
+#
+#       GETS CLOSEST COLOR
+#
+    def get_closest_color(self, rgb_value):
+        chosen_pixel = rgb_value
+        available_pixel = self.available_pixel
+        distances = []
+
+        for key, value in available_pixel.items():
+            a1 = np.asarray(value)
+            c1 = np.asarray(chosen_pixel)
+            curr_dist = np.linalg.norm(a1 - c1)
+            distances += [curr_dist]
+            if(curr_dist == min(distances)):
+                curr_key = key
+
+        return curr_key
 
 
+
+
+tmp = uart_api()
+print (tmp.get_closest_color([200,0,150]))
