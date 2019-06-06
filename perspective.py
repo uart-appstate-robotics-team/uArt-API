6bedb64557d790db1817504c85ba1c7c38de0adf
JacobJustice <jacobjustice@gmail.com>
2019-06-05 22:24:19 -0400

    added the perspective transform, an images directory, some new attributes as well as removed unnecessary lines of code

---
+++ perspective.py
@@ -0,0 +1,97 @@
+# USAGE
+# python click_and_crop.py --image jurassic_park_kitchen.jpg
+
+# import the necessary packages
+import argparse
+import cv2
+import numpy as np
+
+class PerspectiveTransform:
+	def __init__(self, image):
+		width,height,channels = image.shape
+		self.selection = cv2.resize(cv2.imread("./images/1x1.png"), (height, width))
+		print(self.selection)
+		self.image = image
+		self.warped = None
+		self.points = []
+		self.set_corners()
+		self.perspective_transform()
+
+
+	def order_points(self, pts):
+		rect = np.zeros((4, 2), dtype = "float32")
+		s = np.sum(pts,axis = 1)
+		rect[0] = pts[np.argmin(s)]
+		rect[2] = pts[np.argmax(s)]
+		diff = np.diff(pts, axis = 1)
+		rect[1] = pts[np.argmin(diff)]
+		rect[3] = pts[np.argmax(diff)]
+		return rect
+	
+	def click_and_place(event, x, y, flags, self):
+		if event == cv2.EVENT_LBUTTONDOWN and len(self.points) < 4:
+	        	print("click!")
+	        	self.points.append((x,y))
+	        	print(self.points)
+	        	cv2.circle(self.selection, self.points[-1], 5, (0,0,255), -1)
+
+	def set_corners(self):
+		clone = self.image.copy()
+		sclone = self.selection.copy()
+		added = self.selection.copy()
+
+		cv2.namedWindow("image")
+		#cv2.setMouseCallback("image", click_and_crop)
+		cv2.setMouseCallback("image", PerspectiveTransform.click_and_place, self)
+		# keep looping until the 'c' key is pressed
+		lines = False
+		while True:
+			# display the image and wait for a keypress
+			if len(self.points) == 4 and lines == False:
+				self.selection = sclone.copy()
+				rect = self.order_points(self.points)
+				cv2.line(self.selection,tuple(rect[0]),tuple(rect[1]),(0,255,0), thickness=3, lineType=8)
+				cv2.line(self.selection,tuple(rect[1]),tuple(rect[2]),(0,255,0), thickness=3, lineType=8)
+				cv2.line(self.selection,tuple(rect[2]),tuple(rect[3]),(0,255,0), thickness=3, lineType=8)
+				cv2.line(self.selection,tuple(rect[3]),tuple(rect[0]),(0,255,0), thickness=3, lineType=8)
+				lines = True
+			cv2.addWeighted(self.selection, .4, self.image, 0.6, 0, added)
+			cv2.imshow("image", added)
+			key = cv2.waitKey(1) & 0xFF
+			# if the 'r' key is pressed, reset the cropping region
+			if key == ord("r"):
+				self.image = clone.copy()
+				self.points.clear()
+				selection = cv2.resize(cv2.imread("./images/1x1.png"), (height, width))	# if the 'c' key is pressed, break from the loop
+				lines = False
+			elif key == ord("c"):
+				break
+
+	def perspective_transform(self):
+		print(self.points)
+		rect = self.order_points(self.points)
+		(tl, tr, br, bl) = rect
+		widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+		widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+		maxWidth = max(int(widthA), int(widthB))
+		heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+		heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+		maxHeight = max(int(heightA), int(heightB))
+
+		dst = np.array([
+			[0,0],
+			[maxWidth-1,0],
+			[maxWidth-1, maxHeight-1],
+			[0,maxHeight-1]], dtype="float32")
+		M = cv2.getPerspectiveTransform(rect, dst)
+		self.warped = cv2.warpPerspective(self.image, M, (maxWidth, maxHeight))
+
+		
+	def update_warped(self,image):
+    		self.image = image
+    		self.perspective_transform()
+
+	def get_warped(self):
+		return self.warped
+
+
--- uart.py
+++ uart.py
@@ -3,34 +3,45 @@ import os
 import sys
 import time
 import threading
-#sys.path.append(os.path.join(os.path.dirname(_file_), '../../..'))
-from uarm.wrapper import SwiftAPI
-from uarm.tools.list_ports import get_ports
 
+import perspective
+
+import cv2
+
+#sys.path.append(os.path.join(os.path.dirname(_file_), '../../..'))
+#from uarm.wrapper import SwiftAPI
+#from uarm.tools.list_ports import get_ports
 
 class uart_api:
     available_pixel = {}
     swift = None
     device_info = None
     firmware_version = None
-    
-    def __init__(self):
+    image = None #image you're trying to paint
+    canvas = None #image of the canvas as you're working on it
+    canvas_corners = None #points of the four corners of the canvas (in robot arm coords)
+    ptransform = None #contains the warped image of 
+    def __init__(self, im):
         self.available_pixel = {'red':[255,0,0], 'green':[0,255,0], 'blue':[0,0,255],'magenta':[255,0,255], 'tomato':[255,99,71], 'lawn green':[124,252,0]}
-        self.swift = SwfitAPI(filters={'hwid': 'USB VID:PID=2341:0042'})
+        self.swift = SwiftAPI(filters={'hwid': 'USB VID:PID=2341:0042'})
         self.device_info = swift.get_device_info()
         self.firmware_version = device_info['firmware_version']
-        self.swift.set_mode(0)        
+        self.swift.set_mode(0)
+        self.image = im
+        self.canvas_corners = self.setFourCorners()
+        _, cap = cv2.VideoCapture(0).read()
+        self.ptransform = perspective.PerspectiveTransform(self.image)
+        
 #
 #	HEAT MAP
 #
-    def generate_heatmap(image, canvas):
-        image = image.astype(dtype='int32')
-        canvas = canvas.astype(dtype='int32')
+    def generate_heatmap(self):
+        image = self.image.astype(dtype='int32')
+        canvas = self.canvas.astype(dtype='int32')
 
         subtraction = np.subtract(image,canvas)
         print(subtraction)
 
-        im = np.asarray(Image.open('images/Future-human-Face.png').convert('RGB'))
         heatmap = np.full(im.shape,255, dtype='uint8')
 
         print(heatmap.shape)
@@ -48,8 +59,7 @@ class uart_api:
 #
 #       GETS CLOSEST COLOR
 #
-    def get_closest_color(self, rgb_value):
-        chosen_pixel = rgb_value
+    def get_closest_color(self, chosen_pixel):
         available_pixel = self.available_pixel
         distances = []
 
@@ -68,10 +78,10 @@ class uart_api:
 #
 # GoTOList
 #
-    def moveTo():
+    def move_to_file(filename):
         var = []
         count = 0
-        lines = open("uArmCoordinates.txt", "r").read().split('\n')
+        lines = open(filename, "r").read().split('\n')
         x,y,z,f = 0
 
         for i in range(len(lines)):
@@ -84,7 +94,7 @@ class uart_api:
                     z = float(word[1:])
                 elif(word[0] is 'F'):
                     f = float(word[1:])
-            swift.set_position(x=x, y=y, z=z, speed =f, cmd = "G0")
+            self.swift.set_position(x=x, y=y, z=z, speed =f, cmd = "G0")
             time.sleep(1)
         coordinates.close()
 
@@ -100,22 +110,22 @@ class uart_api:
          while todo >0:
              key = input()
              if key == "tr":
-                 newCoord = swift.get_position()
+                 newCoord = self.swift.get_position()
                  coords[1] = newCoord
                  todo -= 1
                  print("Top right coordinate saved as ", newCoord)
              elif key == "tl":
-                 newCoord = swift.get_position()
+                 newCoord = self.swift.get_position()
                  coords[0] = newCoord
                  todo -= 1
                  print("Top left coordinate saved as", newCoord)
              elif key == "bl":
-                 newCoord = swift.get_position()
+                 newCoord = self.swift.get_position()
                  coords[2] = newCoord
                  todo -= 1
                  print("Bottom left coordinate saved as", newCoord)
              elif key == "br":
-                 newCoord = swift.get_position()
+                 newCoord = self.swift.get_position()
                  coords[3] = newCoord
                  todo -= 1
                  print("Bottom right coodirnate saved as", newCoord)
@@ -128,11 +138,9 @@ class uart_api:
 # SAVED COORDS TO FILE
 #
     def saveCoordsToFile(self):
-        spped_s = 10000
         delay = 1
         cmd_s = 'G0'
 
-        todo = 4
         coords = []
         while True:
             key = input()
@@ -151,3 +159,8 @@ class uart_api:
         coordinates.close()
         moveTo("Coordinates.txt")
         return coords
+
+_,im = cv2.VideoCapture(0).read()
+uart = uart_api(im)
+
+
