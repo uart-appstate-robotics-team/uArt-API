cacd513ad02ea49308aae440cec21f3b6d80cb3b
Jacob Justice <jacobjustice@gmail.com>
2019-06-25 23:15:08 +0000

    added a new __init__

--- uart.py
+++ uart.py
@@ -8,6 +8,8 @@ import perspective
 
 import cv2
 
+from PIL import Image
+
 #sys.path.append(os.path.join(os.path.dirname(_file_), '../../..'))
 from uarm.wrapper import SwiftAPI
 from uarm.tools.list_ports import get_ports
@@ -31,11 +33,29 @@ class uart_api:
         self.image = im
         print("Setting four corners; input tl, tr, bl or br")
         self.canvas_corners = self.setFourCorners()
+        #self.canvas_corners =  [[271.41, 100.41, 160.48], [271.48, -88.76, 162.2], [196.59, 85.7, -97.66], [195.83, -82.88, -90.02]]
+
         _, cap = cv2.VideoCapture(0).read()
         self.ptransform = perspective.PerspectiveTransform(self.image)        
         self.M = self.get_m()
         print("Arm all set up!")
 
+    #    if you pass in a third parameter the device won't require any calibration and instead use default values for everything
+    def __init__(self, im, default):
+        self.available_pixel = {'red':[255,0,0], 'green':[0,255,0], 'blue':[0,0,255],'magenta':[255,0,255], 'tomato':[255,99,71], 'lawn green':[124,252,0]}
+        self.swift = SwiftAPI(filters={'hwid': 'USB VID:PID=2341:0042'})
+        self.device_info = self.swift.get_device_info()
+        self.firmware_version = self.device_info['firmware_version']
+        self.swift.set_mode(0)
+        self.image = im
+        print("Setting four corners; input tl, tr, bl or br")
+        #self.canvas_corners = self.setFourCorners()
+        self.canvas_corners =  [[271.41, 100.41, 160.48], [271.48, -88.76, 162.2], [196.59, 85.7, -97.66], [195.83, -82.88, -90.02]]
+
+        _, cap = cv2.VideoCapture(0).read()
+        self.ptransform = perspective.PerspectiveTransform(self.image)        
+        self.M = self.get_m()
+        print("Arm all set up!")
 #
 #	HEAT MAP
 #
@@ -94,7 +114,7 @@ class uart_api:
                     x = float(word[1:])
                 elif(word[0] is 'Y'):
                     y = float(word[1:])
-              elif(word[0] is 'Z'):
+                elif(word[0] is 'Z'):
                     z = float(word[1:])
                 elif(word[0] is 'F'):
                     f = float(word[1:])
@@ -193,7 +213,7 @@ class uart_api:
 #
     def go_to_position(self, xyz, f):
         self.swift.set_position(x=xyz[0], y=xyz[1], z=xyz[2], speed = f, cmd = "G0")
-        time.sleep(10)
+#:        time.sleep(1)
 
 #
 #    draw a line
@@ -218,26 +238,53 @@ class uart_api:
     def draw_line2(self, points):
         startxyz = self.xy_to_xyz(points[0])
         endxyz = self.xy_to_xyz(points[-1])
-        start_pre = [startxyz[0]-50, startxyz[1], startxyz[2]]
-        end_post = [endxyz[0]-50, endxyz[1], endxyz[2]]
+        start_pre = [startxyz[0]-20, startxyz[1], startxyz[2]]
+        end_post = [endxyz[0]-20, endxyz[1], endxyz[2]]
 
         print("going to start pre")
         self.go_to_position(start_pre, 10000)
         for point in points:
             point_xyz = self.xy_to_xyz(point)
-            self.go_to_position(point_xyz), 5000)
-        print("going to start")
-        self.go_to_position(startxyz, 5000)
-        print("going to end")
-        self.go_to_position(endxyz, 5000)
-            
+            self.go_to_position(point_xyz, 5000)
         print("going to end post")
         self.go_to_position(end_post, 10000)
 
-
 image = cv2.resize(cv2.imread("./images/1x1.png"), (200,200))
 uart = uart_api(image)
-while True:
-    start = [int(input("input x for start")),int(input("input y for start"))]
-    end = [int(input("input x for end")),int(input("input y for end"))]
-    uart.draw_line(start,end)
+
+print("head")
+uart.draw_line2([[139,61],[159,61],[166,52],[166,38],[159,33],[141,33],[132,38],[132,55],[139,61]])
+
+print("neck")
+uart.draw_line2([[148,63],[148,71]])
+
+print("body")
+uart.draw_line2([[148,71],[148,122]])
+
+print("right leg")
+uart.draw_line2([[148,122],[175,183]])
+
+print("left leg")
+uart.draw_line2([[148,122],[121,183]])
+
+print("left arm")
+uart.draw_line2([[148,71],[114,104]])
+
+print("right arm")
+uart.draw_line2([[148,71],[176,108]])
+
+asdf = input("go y/n")
+
+uart.draw_line2([[139,61],[159,61],[166,52],[166,38],[159,33],[141,33],[132,38],[132,55],[139,61]])
+
+uart.draw_line2([[148,63],[148,71]])
+
+uart.draw_line2([[148,71],[148,122]])
+
+uart.draw_line2([[148,122],[121,183]])
+
+uart.draw_line2([[148,122],[175,183]])
+
+uart.draw_line2([[148,71],[114,104]])
+
+uart.draw_line2([[148,71],[176,108]])
